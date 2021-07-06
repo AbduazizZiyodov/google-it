679af670dc2a6d2f84c6424b69ca9b0cbe967a32
Abduaziz <abduaziz.ziyodov@mail.ru>
2021-07-07 01:36:48 +0500

    :pencil: added some comments ...

--- core.py
+++ core.py
@@ -2,7 +2,7 @@ from os import getenv
 from dotenv import load_dotenv
 from googleapiclient.discovery import build
 
-
+# For getting enviroment variables from .env file
 load_dotenv()
 
 API_KEY: str = getenv('GOOGLE_API_KEY')
@@ -10,11 +10,19 @@ CSE: str = getenv('CSE_ID')
 
 
 def search(search_term: str, api_key: str, cse_id: str, **kwargs) -> list:
+    """
+    - search_term : no comment
+    - api_key : from https://developers.google.com/custom-search/v1/introduction (click get API key)
+    - cse_id : after creating search engine, you can get it from its settings page
+    """
     service = build("customsearch", "v1", developerKey=api_key)
     response = service.cse().list(q=search_term, cx=cse_id, **kwargs).execute()
     return response['items']
 
 
 def get_results(term: str = "google") -> list:
+    """
+    Default search term is 'google'
+    """
     results = search(term, API_KEY, CSE, num=10)
     return [result['link'] for result in results]
