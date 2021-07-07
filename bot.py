fab944f5c1dbd667b64bd351606dc02fc7a9efb2
Abduaziz <abduaziz.ziyodov@mail.ru>
2021-07-08 02:24:03 +0500

    :pencil: Changed import method , improved checking functionality

--- bot.py
+++ bot.py
@@ -1,25 +1,30 @@
-import telebot
+from telebot import TeleBot
 
-from settings import TELEGRAM_API_TOKEN
+from telebot.types import InlineQuery
+from telebot.types import InlineQueryResultArticle
+from telebot.types import InputTextMessageContent
+
+from utils import check
 from utils import get_search_results
 
+from settings import TELEGRAM_API_TOKEN
 
-bot = telebot.TeleBot(TELEGRAM_API_TOKEN)
 
-blacklist: list = []
+bot = TeleBot(TELEGRAM_API_TOKEN)
 
 
 @bot.inline_handler(func=lambda query: len(query.query) > 0)
-def perform_search(query):
-    results: str = get_search_results(query=query)
-    if query.query.lower() in blacklist:
-        results = "Meni ishga tushurgan ushbu foydalanuvchi yomon so'z aytdi😀"
-    content = telebot.types.InlineQueryResultArticle(
+def perform_search(query: InlineQuery):
+    if check(query):
+        results: str = get_search_results(query)
+    else:
+        results: str = '💥'
+    content = InlineQueryResultArticle(
         id='1',
-        title=f'The search for "{query.query}" is done ✅',
-        description='Click me 😊',
-        input_message_content=telebot.types.InputTextMessageContent(
-                    message_text=results)
+        title='The search for "{}" is done ✅'.format(query.query),
+        description='Click me if you want to see results😊',
+        input_message_content=InputTextMessageContent(
+            message_text=results)
     )
     bot.answer_inline_query(query.id, [content])
 
