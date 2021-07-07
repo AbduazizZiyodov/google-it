176e2b90655182b4c35cc086ee69a21e43c5fcd1
Abduaziz <abduaziz.ziyodov@mail.ru>
2021-07-08 02:01:22 +0500

    :100: Added Core functionality of bot
    - with telebot )

--- bot.py
+++ bot.py
@@ -1,6 +1,43 @@
-def main():
-    print('hello i am bot!')
+import telebot
+
+from os import getenv
+
+from dotenv import load_dotenv
+
+from core import googleit
 
 
-if __name__ == "__main__":
-    main()
+load_dotenv()
+
+TELEGRAM_API_TOKEN: str = getenv("TELEGRAM_API_TOKEN")
+bot = telebot.TeleBot(TELEGRAM_API_TOKEN)
+
+blacklist: list = []
+
+
+def get_search_results(query: telebot.types.InlineQuery) -> str:
+    query, results = query.query.lower(), "[🔍Search results🤓]\n\n"
+    total: int = 0
+    for index, result in enumerate(googleit(term=query)):
+        results += f'🔰{index+1}) {result}\n'
+        total += 1
+    return results
+
+
+@bot.inline_handler(func=lambda query: len(query.query) > 0)
+def perform_search(query):
+    results: str = get_search_results(query=query)
+    if query.query.lower() in blacklist:
+        results = "Meni ishga tushurgan ushbu foydalanuvchi yomon so'z aytdi😀"
+    content = telebot.types.InlineQueryResultArticle(
+        id='1',
+        title=f'The search for "{query.query}" is done ✅',
+        description='Click me 😊',
+        input_message_content=telebot.types.InputTextMessageContent(
+                    message_text=results)
+    )
+    bot.answer_inline_query(query.id, [content])
+
+
+if __name__ == '__main__':
+    bot.infinity_polling()
