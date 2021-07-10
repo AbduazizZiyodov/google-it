f570a809dbc999725940bc9a9cebb3c2e008651d
Abduaziz <abduaziz.ziyodov@mail.ru>
2021-07-11 01:57:53 +0500

    🔰Settings moved in one dictionary named config

--- settings.py
+++ settings.py
@@ -3,7 +3,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-TELEGRAM_API_TOKEN: str = getenv("TELEGRAM_API_TOKEN")
+config: dict = {
+    "BOT_TOKEN": getenv("TELEGRAM_API_TOKEN"),
 
-GOOGLE_API_KEY: str = getenv('GOOGLE_API_KEY')
-CSE_ID: str = getenv('CSE_ID')
+    "GOOGLE_API_KEY": getenv('GOOGLE_API_KEY'),
+    "CSE_ID": getenv('CSE_ID'),
+
+    "GROUP_CHAT_ID": getenv("GROUP_CHAT_ID"),
+}
