9d8dd9b2c7aa4658d0397169a62f93b54052d7cf
Abduaziz <abduaziz.ziyodov@mail.ru>
2021-07-11 01:56:19 +0500

    :rocket: Added checking group member functionality

--- bot.py
+++ bot.py
@@ -2,38 +2,35 @@ from time import sleep
 from telebot import TeleBot
 
 from telebot.types import InlineQuery
-from telebot.types import InlineQueryResultArticle
-from telebot.types import InputTextMessageContent
 
 from utils import save
 from utils import check
 from utils import activity
 from utils import get_search_results
+from utils import make_query_result
+from utils import make_chat_member_result
+from utils import check_group_member
 
-from settings import TELEGRAM_API_TOKEN
+from settings import config
 
 
-bot = TeleBot(TELEGRAM_API_TOKEN)
+bot = TeleBot(config["BOT_TOKEN"])
 
 activity.log('Bot works!')
 
 
 @bot.inline_handler(func=lambda query: len(query.query) > 0)
 def perform_search(query: InlineQuery):
-    sleep(3)
-    if check(query):
-        results: str = get_search_results(query)
+    print(check_group_member(query.from_user.id))
+    if check_group_member(query.from_user.id):
+        sleep(3)
+        results: str = get_search_results(query) if check(query) else '💥'
+        save(query)
+        bot.answer_inline_query(query.id, [make_query_result(results, query)])
     else:
-        results: str = '💥'
-    content = InlineQueryResultArticle(
-        id='1',
-        title='The search for "{}" is done ✅'.format(query.query),
-        description='Click me if you want to see results😊',
-        input_message_content=InputTextMessageContent(
-            message_text=results)
-    )
-    save(query)
-    bot.answer_inline_query(query.id, [content])
+        save(query)
+        bot.answer_inline_query(
+            query.id, [make_chat_member_result(config["GROUP_CHAT_ID"])])
 
 
 if __name__ == '__main__':
