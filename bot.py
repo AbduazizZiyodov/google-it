632b05eec4203f37ab3e698ca420fb45e27f9652
Abduaziz <abduaziz.ziyodov@mail.ru>
2021-07-10 23:58:30 +0500

    :fire: Added logging functionality

--- bot.py
+++ bot.py
@@ -1,10 +1,13 @@
+from time import sleep
 from telebot import TeleBot
 
 from telebot.types import InlineQuery
 from telebot.types import InlineQueryResultArticle
 from telebot.types import InputTextMessageContent
 
+from utils import save
 from utils import check
+from utils import activity
 from utils import get_search_results
 
 from settings import TELEGRAM_API_TOKEN
@@ -12,9 +15,12 @@ from settings import TELEGRAM_API_TOKEN
 
 bot = TeleBot(TELEGRAM_API_TOKEN)
 
+activity.log('Bot works!')
+
 
 @bot.inline_handler(func=lambda query: len(query.query) > 0)
 def perform_search(query: InlineQuery):
+    sleep(3)
     if check(query):
         results: str = get_search_results(query)
     else:
@@ -26,6 +32,7 @@ def perform_search(query: InlineQuery):
         input_message_content=InputTextMessageContent(
             message_text=results)
     )
+    save(query)
     bot.answer_inline_query(query.id, [content])
 
 
--- utils.py
+++ utils.py
@@ -1,4 +1,7 @@
+from loguru import logger
+
 from telebot.types import InlineQuery
+
 from core import googleit
 
 
@@ -18,3 +21,23 @@ def check(query: InlineQuery) -> bool:
         if word in term:
             return False
     return True
+
+
+class Logging(object):
+    def __init__(self, file) -> None:
+        self.file = file
+        self.__setup_logger()
+
+    def __setup_logger(self) -> None:
+        logger.add(self.file, backtrace=True, diagnose=True)
+
+    def log(self, message: str) -> None:
+        logger.info(message)
+
+
+activity = Logging('actions.log')
+
+
+def save(query: InlineQuery) -> None:
+    activity.log(
+        f"Search Term **{query.query}** By user -> first_name {query.from_user.first_name} | last_name {query.from_user.last_name} | username @{query.from_user.username} | user_id #{query.from_user.id}")
