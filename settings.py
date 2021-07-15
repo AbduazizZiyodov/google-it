c233223a83e71156b38f9bc0a75ab497151a9275
Abduaziz <abduaziz.ziyodov@mail.ru>
2021-07-16 01:10:56 +0500

    :repeat: Update settings, added new variable

--- settings.py
+++ settings.py
@@ -3,11 +3,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-config: dict = {
-    "BOT_TOKEN": getenv("TELEGRAM_API_TOKEN"),
+BOT_TOKEN = getenv("TELEGRAM_API_TOKEN")
+GROUP_CHAT_ID = getenv("GROUP_CHAT_ID")
+CHANNEL_NAME = getenv("CHANNEL_NAME")
 
-    "GOOGLE_API_KEY": getenv('GOOGLE_API_KEY'),
-    "CSE_ID": getenv('CSE_ID'),
-
-    "GROUP_CHAT_ID": getenv("GROUP_CHAT_ID"),
-}
+GOOGLE_API_KEY = getenv('GOOGLE_API_KEY')
+CSE_ID = getenv('CSE_ID')
