4706a2d28a3569b4ee1ad5c7a4d2ca74a78857bb
Abduaziz <abduaziz.ziyodov@mail.ru>
2021-07-16 01:13:44 +0500

    :wave: Added `/start` command handler

--- bot.py
+++ bot.py
@@ -1,27 +1,37 @@
 from time import sleep
-from telebot import TeleBot
 
+from telebot import TeleBot
+from telebot.types import Message
 from telebot.types import InlineQuery
+
+from settings import BOT_TOKEN
+from settings import GROUP_CHAT_ID
 
 from utils import save
 from utils import check
 from utils import activity
-from utils import get_search_results
+from utils import get_random_meme
 from utils import make_query_result
-from utils import make_chat_member_result
+from utils import get_search_results
 from utils import check_group_member
-
-from settings import config
-
-
-bot = TeleBot(config["BOT_TOKEN"])
-
-activity.log('Bot works!')
+from utils import make_chat_member_result
 
 
-@bot.inline_handler(func=lambda query: len(query.query) > 0)
+bot = TeleBot(BOT_TOKEN)
+
+print('Bot is started!')
+
+
+@bot.message_handler(['start'])
+def welcome(message: Message):
+    image = open('images/{}'.format(get_random_meme()), 'rb')
+    bot.send_photo(chat_id=message.chat.id, photo=image,
+                   caption="Learn to search from Google🙂\n#random_meme")
+    image.close()
+
+
+@bot.inline_handler(func=lambda query: len(query.query) > 7)
 def perform_search(query: InlineQuery):
-    print(check_group_member(query.from_user.id))
     if check_group_member(query.from_user.id):
         sleep(3)
         results: str = get_search_results(query) if check(query) else '💥'
@@ -30,7 +40,7 @@ def perform_search(query: InlineQuery):
     else:
         save(query)
         bot.answer_inline_query(
-            query.id, [make_chat_member_result(config["GROUP_CHAT_ID"])])
+            query.id, [make_chat_member_result(GROUP_CHAT_ID)])
 
 
 if __name__ == '__main__':
