acc54342d1808ec25ff5fed2699c17d6f7d96ead
Abduaziz <abduaziz.ziyodov@mail.ru>
2021-07-18 00:38:27 +0500

    :white_check_mark: Changed message length limit

--- bot.py
+++ bot.py
@@ -4,12 +4,11 @@ from telebot import TeleBot
 from telebot.types import Message
 from telebot.types import InlineQuery
 
-from settings import BOT_TOKEN
-from settings import GROUP_CHAT_ID
+from settings import BOT_TOKEN as token
+from settings import GROUP_CHAT_ID as chat_id
 
 from utils import save
 from utils import check
-from utils import activity
 from utils import get_random_meme
 from utils import make_query_result
 from utils import get_search_results
@@ -17,22 +16,22 @@ from utils import check_group_member
 from utils import make_chat_member_result
 
 
-bot = TeleBot(BOT_TOKEN)
+bot = TeleBot(token)
 
 print('Bot is started!')
 
 
 @bot.message_handler(['start'])
-def welcome(message: Message):
+def welcome(message: Message) -> None:
     image = open('images/{}'.format(get_random_meme()), 'rb')
     bot.send_photo(chat_id=message.chat.id, photo=image,
                    caption="Learn to search from Google🙂\n#random_meme")
     image.close()
 
 
-@bot.inline_handler(func=lambda query: len(query.query) > 7)
-def perform_search(query: InlineQuery):
-    if check_group_member(query.from_user.id):
+@bot.inline_handler(lambda query: len(query.query) > 5)
+def perform_search(query: InlineQuery) -> None:
+    if check_group_member(bot, query.from_user.id):
         sleep(3)
         results: str = get_search_results(query) if check(query) else '💥'
         save(query)
@@ -40,7 +39,7 @@ def perform_search(query: InlineQuery):
     else:
         save(query)
         bot.answer_inline_query(
-            query.id, [make_chat_member_result(GROUP_CHAT_ID)])
+            query.id, [make_chat_member_result()])
 
 
 if __name__ == '__main__':
