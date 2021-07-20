c13815c976a45dedaa49dff487969e14bbfea42a
Abduaziz <abduaziz.ziyodov@mail.ru>
2021-07-20 17:50:09 +0500

    :+1: Update settings and requirements

--- settings.py
+++ settings.py
@@ -7,5 +7,9 @@ BOT_TOKEN = getenv("TELEGRAM_API_TOKEN")
 GROUP_CHAT_ID = getenv("GROUP_CHAT_ID")
 CHANNEL_NAME = getenv("CHANNEL_NAME")
 
+SUPER_USER_ID = getenv("SUPER_USER_ID")  # sudo :)
+
 GOOGLE_API_KEY = getenv('GOOGLE_API_KEY')
 CSE_ID = getenv('CSE_ID')
+
+SENTRY_DSN = getenv("SENTRY_SDK")
