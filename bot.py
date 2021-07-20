f3cbe216095a2b91677d39e64372312f60263d4e
Abduaziz <abduaziz.ziyodov@mail.ru>
2021-07-20 17:50:32 +0500

    :+1: Sentry initalization

--- bot.py
+++ bot.py
@@ -1,11 +1,13 @@
 from time import sleep
 
+from sentry_sdk import init
+
 from telebot import TeleBot
 from telebot.types import Message
 from telebot.types import InlineQuery
 
 from settings import BOT_TOKEN as token
-from settings import GROUP_CHAT_ID as chat_id
+from settings import SENTRY_DSN
 
 from utils import save
 from utils import check
@@ -17,6 +19,7 @@ from utils import make_chat_member_result
 
 
 bot = TeleBot(token)
+init(SENTRY_DSN)
 
 print('Bot is started!')
 
@@ -31,16 +34,16 @@ def welcome(message: Message) -> None:
 
 @bot.inline_handler(lambda query: len(query.query) > 5)
 def perform_search(query: InlineQuery) -> None:
+    save(query, bot)
     if check_group_member(bot, query.from_user.id):
         sleep(3)
         results: str = get_search_results(query) if check(query) else '💥'
-        save(query)
         bot.answer_inline_query(query.id, [make_query_result(results, query)])
     else:
-        save(query)
         bot.answer_inline_query(
             query.id, [make_chat_member_result()])
 
 
 if __name__ == '__main__':
     bot.infinity_polling()
+
